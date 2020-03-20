3c05c119a015f4ef05cbf1712107e4301742d718
Brent Wallace <brent@bdw.dev>
2020-03-20 14:00:06 -0400

    Initial commit

---
+++ routers/routes.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const sgMail = require('@sendgrid/mail');
+const { check, validationResult } = require('express-validator');
+
+const router = new express.Router();
+
+router.get('/', (req, res) => {
+  res.render('index.hbs', {
+    title: 'Park Visalia Memory Care & Assisted Living',
+  });
+});
+
+router.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    title: 'About Park Visalia',
+  });
+});
+
+router.get('/photos', (req, res) => {
+  res.render('photos.hbs', {
+    title: 'Photo Gallery',
+  });
+});
+
+router.get('/floor-plans', (req, res) => {
+  res.render('floor-plans.hbs', {
+    title: 'Floorplans',
+  });
+});
+
+router.get('/assisted-living', (req, res) => {
+  res.render('assisted-living.hbs', {
+    title: 'Assisted Living',
+  });
+});
+
+router.get('/memory-care', (req, res) => {
+  res.render('memory-care.hbs', {
+    title: 'Memory Care',
+  });
+});
+
+router.get('/activities', (req, res) => {
+  res.render('activities.hbs', {
+    title: 'Events & Activities',
+  });
+});
+
+router.get('/covid-19', (req, res) => {
+  res.render('covid.hbs', {
+    title: 'COVID-19',
+  });
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact.hbs', {
+    title: 'Contact Us',
+  });
+});
+
+router.post('/contact', [
+  check('fromEmail').isEmail().normalizeEmail(),
+  check('firstName').trim().escape(),
+  check('lastName').trim().escape().notEmpty(),
+  check('phone').isMobilePhone('en-US'),
+  check('referralSource').trim().escape(),
+  check('inquiringFor').trim().escape(),
+  check('brochure').trim().escape(),
+  check('tour').trim().escape(),
+  check('comments').trim().escape(),
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).send();
+  }
+  const toEmail = process.env.EMAIL_RECIPIENT.split(',');
+  const { fromEmail, firstName, lastName, phone, referralSource, inquiringFor, brochure, tour, comments } = req.body;
+  const msg = {
+    to: toEmail,
+    from: fromEmail,
+    subject: `Park Visalia Contact Form: ${firstName} ${lastName} - ${fromEmail}`,
+    text: `${firstName} ${lastName} has submitted an inquiry through the contact form on parkvisalia.com. They can be contacted by phone at ${phone} and by email at ${fromEmail}.\r\nInquiring for:${inquiringFor}\r\nReferred by: ${referralSource}\r\nBrochure:${brochure}\r\nTour:${tour}\r\n\r\nMessage start: ${comments}`,
+    html: `
+      <h1>New Contact Form Submission</h1>
+        <p><strong>${firstName} ${lastName}</strong> has submitted an inquiry through the contact form on parkvisalia.com.</p>
+        <h2>Contact Details</h2>
+        <ul>
+          <li>Email: ${fromEmail}</li>
+          <li>Phone: ${phone}</li>
+          <li>Referred by: ${referralSource}</li>
+          <li>Inquiring for: ${inquiringFor}</li>
+          <li>Brochure: ${brochure}</li>
+          <li>Tour: ${tour}</li>
+        </ul>
+        <h2>Message Start:</h2>
+        <p>${comments}</p>
+        `,
+  };
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  sgMail.send(msg);
+  return res.send('Thank you for your inquiry! Our Community Relations Director will reach out to you shortly.');
+});
+
+module.exports = router;
---
+++ src/app.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const helmet = require('helmet');
+const router = require('../routers/routes');
+require('dotenv').config();
+
+const app = express();
+app.use(helmet());
+
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(router);
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server listening on ${port}`);
+});
