d7b2767e209d997c460b605915d82fa8853b1529
Brent Wallace <brent@bdw.dev>
2020-04-24 11:47:59 -0400

    Added 404 handler

--- src/app.js
+++ src/app.js
@@ -16,6 +16,9 @@ app.use(express.static(publicDirectoryPath));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
+app.use((req, res, next) => {
+  res.status(404).send('Error: 404 - Not Found');
+});
 
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
